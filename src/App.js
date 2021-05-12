1e0de605c8907b8a21555f9de0340da72a669596
Rahul Kumar <rahul7k7@gmail.com>
2021-05-12 17:29:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
-    <p>E-Commerce Jersey Store Application Publishing Soon</p>
+    <>
+      <Header />
+      <main>
+        <p>E-Commerce Jersey Store Application Publishing Soon</p>
+      </main>
+      <Footer />
+    </>
   );
 }
 
