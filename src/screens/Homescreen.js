88fe88ca6f2bbcb1a3f2df0af0c7084d3bc763af
Rahul Kumar <rahul7k7@gmail.com>
2021-05-13 12:43:37 +0530

    home screen

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import Product from '../components/Product';
 import products from "../products";
 
 const Homescreen = () => {
   return (
     <>
-      <h1>Club Football Jerseys</h1>
+      <h1>Club-Football Jerseys</h1>
       <Row>
          {products.map(product => (
              <Col sm={12} md={6} lg={4} xl={3}>
-                 <h3>{product.name}</h3>
+                 <Product product={product} />
              </Col>
          ))} 
       </Row>
